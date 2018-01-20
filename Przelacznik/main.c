da2d17391b4690d48fe053a2675b93af074ae30b
Grzegorz Prawdzik <g.prawdzik@gmail.com>
2018-01-20 20:49:00 +0100

    przelacznik

--- Przelacznik/main.c
+++ Przelacznik/main.c
@@ -1,6 +1,11 @@
 /*
  * Przelacznik.c
  *
+ 
+ Fuses:
+ high: xff
+ low: x69
+ 
  * Created: 05.01.2018 22:35:48
  * Author : Grzegorz
  */ 
@@ -10,8 +15,12 @@
 #include <avr/sleep.h>
 #include <avr/power.h>
 
-volatile short isEnabled = 0;
+//ile czekaæ na wygaszenie
+#define clockTickWait 600
+
+volatile short isEnabled = 1;
 volatile short isButtonPushed = 0;
+volatile short disableAfterUnPush = 0;
 volatile int counter = 0;
 volatile short buttonCounter = 0;
 
@@ -24,7 +33,7 @@ void inline power_ac_disable() {
 void inline initExtInterrupt(void)
 {//the low level of INTx generates an interrupt request
 	GIMSK |= (1<<INT0);	  //External Interrupt Request 0 Enable
-	GIFR  |= (1<<INTF0);	  //Clear External Interrupt 0 Flag	
+	//GIFR  |= (1<<INTF0);	  //Clear External Interrupt 0 Flag	
 }
 
 void inline disableExtInterrupt(void)
@@ -42,25 +51,22 @@ void sleep()
 {
 	PORTB &= ~(1<<PB3); //wylaczenie
 	counter = 0; //licznik na 0
-	isEnabled = 0;
-	initExtInterrupt(); //wlaczenie zewnetrznych przerwan
-	//set_sleep_mode(SLEEP_MODE_PWR_DOWN); //ustaw tryb spania
-	//sleep_mode();
+	
+	initExtInterrupt();
+
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN); //ustaw tryb spania
+	sleep_mode();
 }
 
 
-//TODO: zablokowaæ usypianie gdy przycisk wlaczony
+
 int main(void)
 {
 	power_adc_disable();
 	power_ac_disable();
 	
-	
 	//Pb3 jako wyjscie
 	DDRB |= (1<<PB3);
-	
-	DDRB |= (1<<PB4);
-	
 	
 	//PB1 jako wejscie z pull-up - tam bêdzie prze³¹cznik
 	DDRB &= ~(1<<PB1);
@@ -69,61 +75,50 @@ int main(void)
 	Timer0Init();
 	
 	sei();
-	//TODO: od razu sleep
+	
 	sleep();
 	
     while (1) 
     {
-		
-		if (counter > 50 && !isButtonPushed) {//TODO: zrobic na 600
-			sleep();
-		}
-		
-		//Je¿eli wy³¹czyliœmy i mamy pewnoœæ, ¿e button zosta³ wciœniêty
-		if (!isButtonPushed && !isEnabled) {
-			sleep();
-		}
-		
-		//wlaczamy zewnetrzne przerwanie gdy jestesmy pewni, ¿e przycisk zosta³ odcisniety	
 		if (!isButtonPushed) {
-			initExtInterrupt();
-		}
+			if (counter > clockTickWait || !isEnabled) {
+				sleep();
+			}
+		}		
     }
 }
 
-//TODO: nie wy³¹cza siê poprawnie przyciskiem - zobaczyæ co jest nie tak - w tej chwili nie mam zielonego pojêcia
 
+//przerwaniem tylko wybudzenie
 ISR(INT0_vect)
 {
 	disableExtInterrupt();
+	PORTB |= (1<<PB3);
 	isButtonPushed = 1;
-	
-	if (!isEnabled) {
-		isEnabled = 1;
-		PORTB |= (1<<PB3);
-	} else {
-		isEnabled = 0;
-	}
+	isEnabled = 1;
+	disableAfterUnPush = 0;
 }
 
-ISR(TIM0_OVF_vect) //przerwanie przepe³nienia timera
+//przerwanie przepe³nienia timera
+ISR(TIM0_OVF_vect) 
 {
 	counter++;
+	
+	if (!isButtonPushed && !(PINB & (1<<PB1))) {
+		isButtonPushed = 1;
+		disableAfterUnPush = 1;
+	}
+
 	if (isButtonPushed && (PINB & (1<<PB1))) {
 		buttonCounter++;
 		if (buttonCounter > 2) {
 			isButtonPushed = 0;
 			buttonCounter = 0;
+			if (disableAfterUnPush) { 
+				isEnabled = 0;
+			}
 		}
 	}
-	
-	if (isButtonPushed) {
-		PORTB |= (1<<PB4);
-		} else {
-		PORTB &= ~(1<<PB4);
-	}
-	
-	
 }
 
 
